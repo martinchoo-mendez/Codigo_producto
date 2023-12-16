cf9eefeee7cb401d5140a85494c9d71c62890441
Martin Mendez <mendez.martinh4@gmail.com>
2023-12-15 22:00:04 -0300

    código final

--- Codigo_producto.py
+++ Codigo_producto.py
@@ -19,35 +19,27 @@ con_producto=""
 voc_producto=""
 código=""
 
-#Primero, separamos el producto en consonantes y vocales
 for char in producto:
     if char in consonantes:
         con_producto+=char
     else:
         voc_producto+=char
 
-#ahora, tomamos las dos consonantes para ponerlas en el código
 c=1
 for char in con_producto:
     if c<=2:
         código+=char.upper()
     c+=1
-#print(código) #primer print, solo para verificar que el código tome las dos vocales.
     
-#ahora, agrego una vocal aleatoria
 vocal=random.choice(voc_producto)
 código+=vocal.lower()
-#print(código) #otro print para verificar.
 
-#agregamos un número random entre 100 y 999
 número=random.randint(100, 999)
 código+=str(número)
-#print(código) #print para verificar.
 
-#por último, agregamos "*" o "#", según la cantidad de carateres.
 cant_caracteres=len(producto)
 if cant_caracteres%2!=0:
     código+="*"
 else:
     código+="#"
-print(código) #este print es el importante, y el único que debe quedar.
+print(código)
