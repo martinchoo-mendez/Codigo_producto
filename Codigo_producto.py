81bc0018a164135f840bc378d260e9aef7dbe3df
Martin Mendez <mendez.martinh4@gmail.com>
2023-12-15 21:33:44 -0300

    reglas para armar el codigo

---
+++ Codigo_producto.py
@@ -0,0 +1,11 @@
+#Una empresa proveedora de alimentos, necesita generar un código para cada producto, según las
+#siguientes reglas:
+
+#-El primer y segundo carácter, deben ser dos consonantes en mayúsculas del nombre del producto.
+#-El tercer carácter, debe ser una vocal aleatoria en minúscula del nombre del producto.
+#-Luego, un número aleatorio entre el 100 y el 999.
+#-El último carácter, debe ser "*" si la cantidad de caracteres del nombre del producto es impar,
+#y en caso contrario, debe ser un "#"
+
+#Se nos pide realizar un programa que, dado el nombre de un producto, genere su respectivo código.
+#Por ejemplo, si el usuario ingresa: "tomates triturados", un código posible sería: TMe853*.
